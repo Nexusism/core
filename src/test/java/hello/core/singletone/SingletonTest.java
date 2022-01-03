1c4165974290123812c337445bbefb56f86c6bf1
Nexusism <wanggoog91@gmail.com>
2022-01-03 18:01:49 +0900

    싱글톤2

--- src/test/java/hello/core/singletone/SingletonTest.java
+++ src/test/java/hello/core/singletone/SingletonTest.java
@@ -1,2 +1,11 @@
-package hello.core.singletone;public class SingletonTest {
+package hello.core.singletone;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pure
 }
